2bc68b3137c08575b9307bf4a0c6b6774d1fb5e3
amorray <developerkyh@gmail.com>
2024-02-01 17:46:33 +0900

    20240201 웹팩 in scss 설정 output 안됨!

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,97 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    mode: 'development',
-    stats: 'errors-only', // 에러가 발생할 때만 출력    //https://webpack.kr/configuration/stats/
-    entry: {
-      app: ['./src/index.js'],
+  mode: 'development',
+  stats: {
+      children: true
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$|jsx/,
-                loader: 'babel-loader',
-                options: {
-                  presets: ['@babel/preset-env', '@babel/preset-react'],
-                },
-            },
-            {
-              test: /\.s[ac]ss$/i,
-              use: ['style-loader', 'css-loader', 'sass-loader']
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './dist/index.html'
-        }),
-        new webpack.LoaderOptionsPlugin({
-            debug:true, // 디버그 쓸 수 있는 공간을 다 디버그로 달아준다는것
-        }),
+    // compress: true,
+    port: 3000,
+    historyApiFallback: true
+  },
+  entry: {
+    app: [
+      './src/index',
+      './src/App',
+      './src/assets/styles/adminPT',
     ],
-    devServer: {
-        historyApiFallback: true
-    }, 
-    externals: {
-        // global app config object
-        config: JSON.stringify({
-            apiUrl: 'http://localhost:4000'
-        })
-    },
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'app.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-        alias: {
-			"@": path.resolve(__dirname, "./src/"),
-		},
-    },
+  },
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'src'), 'node_modules'],
+      extensions: ['.js', '.jsx','.scss'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$|jsx/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              sourceMap: true,
+              url: false,
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  "autoprefixer",
+                ]
+              }
+            }
+          },
+          "sass-loader"
+        ],
+      }
+    ]
+  },
+  plugins: [
+    // Extracts CSS into separate files
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new HtmlWebpackPlugin({
+      filename: './public/index.html',
+      inject: false, // index 주입 false
+      hash: true,
+      minify: { // (https://github.com/kangax/html-minifier)
+        collapseWhitespace: false, // 문서 트리의 텍스트 노드에 공헌하는 공백 축소
+        removeAttributeQuotes: false, // 따옴표
+        removeComments: false // 주석
+      }
+    }),
+  ],
+  externals: {
+    // global app config object
+    config: JSON.stringify({
+        apiUrl: 'http://localhost:4000'
+    })
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'), // 뱉어낼 파일 경로
+    publicPath: '/', // 파일들이 위치할 서버상의 경로
+    filename: 'app.js',
+    clean: true
+  },
 }
